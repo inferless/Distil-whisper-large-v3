98f58cb0eea84eb74d6f728131a132cd1b63e12a
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-28 08:32:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from faster_whisper import WhisperModel
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 class InferlessPythonModel:
-        
     def initialize(self):
-        model_size = "large-v3"
-        self.model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
-    def infer(self, inputs):
-        
-        audio_url = inputs["audio_url"]
-        segments, info = self.model.transcribe(audio_url, beam_size=5)
-        text = ''.join([segment.text for segment in segments])
-        
-        return {"transcribed_output":text}
+        model_id = "distil-whisper/distil-large-v3"
+        model = AutoModelForSpeechSeq2Seq.from_pretrained(model_id, torch_dtype=torch.float16, low_cpu_mem_usage=True, use_safetensors=True)
+        model.to("cuda:0")
+        processor = AutoProcessor.from_pretrained(model_id)
+        self.pipe = pipeline( "automatic-speech-recognition", model=model, tokenizer=processor.tokenizer, feature_extractor=processor.feature_extractor, max_new_tokens=128, torch_dtype=torch.float16, device="cuda:0", )
 
+    def infer(self,inputs):
+        audio_url = inputs["prompt"]
+        pipeline_output = self.pipe(audio_url)
+         
+        return { "transcribed_output" : pipeline_output["text"]}
+        
     def finalize(self):
-        pass
+        self.pipe = None
